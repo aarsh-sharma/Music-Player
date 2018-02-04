6b2a24111e20d9c10733ff18ae1bccdfba252a93
sunidhi chaudhary <sunidhi.nith@gmail.com>
2018-02-04 10:44:11 +0530

    changes

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -9,18 +9,97 @@ import urllib2
 import googlesearch
 import time  
 from time import sleep  
+import player
 #import pygame
 
 fileName = []
+songName = []
 k = 0
-
-window = Tk()
-window.wm_title("Media Player")
-T = Text(window, height=30, width=60)
-T.pack()
-
-pyglet.options['audio'] = ('pulse','openal','silent')
 player = pyglet.media.Player()
+FORWARD_REWIND_JUMP_TIME = 10
+
+def playMusic():
+	player.play()
+	# if source: #lyrics
+	# 	if player.time == source.duration-1:
+	# 		showLyrics()
+
+def pauseMusic():
+	player.pause()
+
+def nextMusic():
+	player.next()
+	if source: # if player.queue not empty
+		showLyrics()
+	
+def add_to_queue(audio_file):
+	global source
+	source = pyglet.media.load(audio_file)
+	player.queue(source)
+	print(source)
+
+def reset_player():
+	player.pause()
+	player.delete()
+
+def is_playing():
+	try:
+		elapsed_time = int(player.time)
+		is_playing = elapsed_time < int(track_length)
+	except:
+		is_playing = False
+	return is_playing
+
+def seek(time):
+	try:
+		player.seek(time)
+	except AttributeError:
+		pass
+
+@property
+def track_length():
+	try:
+		return source.duration
+	except AttributeError:
+		return 0
+
+@property
+def volume():
+	return player.volume
+
+@property
+def elapsed_play_duration():
+	return player.time
+
+# @volume.setter
+# def volume(volume):
+# 	player.volume = volume
+
+def stop():
+	reset_player()
+
+def mute():
+	player.volume = 0.0
+
+def unmute(newvolume_level):
+	player.volume = newvolume_level
+
+def fast_forward():
+	time = player.time + FORWARD_REWIND_JUMP_TIME
+	try:
+		if source.duration > time:
+			seek(time)
+		else:
+			seek(source.duration)
+	except AttributeError:
+		pass
+
+def rewind():
+	time = player.time - FORWARD_REWIND_JUMP_TIME
+	try:
+		seek(time)
+	except:
+		seek(0)
 
 def lyricsScratch(songName):
 	query = songName + ' az lyrics'
@@ -39,53 +118,55 @@ def lyricsScratch(songName):
 
 	parentDiv = soup.find('div',class_ = 'col-xs-12 col-lg-8 text-center')
 	#print parentDiv
-	return list(parentDiv.children)[12].get_text()
-
-def playMusic():
-	# music = pyglet.media.load(fileName)
-	# music.play()
-	# pyglet.app.run()
-	# pygame.mixer.music.play()
-	player.play()
-
-def pauseMusic():
-	# pygame.mixer.music.stop()
-	player.pause()
-
-def nextMusic():
-	player.next()
+	lyrics = list(parentDiv.children)[12].get_text()
+	indexLyricsNotReq = lyrics.find('if  (')
+	if indexLyricsNotReq != -1:
+		lyrics = lyrics[0:indexLyricsNotReq]
+	return lyrics
 
 def browsefunc():
 	global fileName
 	global player
-	global k
-	global songName
+	global songName,k
 	filePath = askopenfilename(parent=window)
 	fileName.append(filePath)
-	music = pyglet.media.load(fileName[k])
-	player.queue(music)
+	add_to_queue(filePath)
+	if k==0:
+		showLyrics()
 
 	# pygame.mixer.music.load(filename[k])
+def showLyrics():
+	global k
 	p = Path(fileName[k])
-	songName = p.name
-	lyrics = lyricsScratch(songName)
+	songName.append(p.name)
+	lyrics = lyricsScratch(songName[k])
 	#print type(lyrics)
-	indexLyricsNotReq = lyrics.find('if  (')
-	if indexLyricsNotReq != -1:
-		lyrics = lyrics[0:indexLyricsNotReq]	
+	T.configure(state='normal')	
+	T.delete('1.0',END)
 	T.insert(END, lyrics)
+	T.configure(state='disabled')
 	k = k+1
 	#print filename
 	
-openSong = Button(window, text ="open", command = browsefunc)
-play = Button(window, text ="play", command = playMusic)
-pause = Button(window, text ="pause", command = pauseMusic)
-next = Button(window, text ="next", command = nextMusic)
+if __name__ == '__main__':
+	window = Tk()
+	window.resizable(width=False, height=False)
+	pyglet.options['audio'] = ('pulse','openal','silent')
+	
+	openSong = Button(window, text ="open", command = browsefunc)
+	play_button=PhotoImage(file='play-button.png')
+	play = Button(window, image = play_button, command = playMusic)
+	pause = Button(window, text ="pause", command = pauseMusic)
+	next = Button(window, text ="next", command = nextMusic)
+
+	T = Text(window,state = 'disabled', height=30, width=60)
+	window.wm_title("Media Player")
+	T.pack()
+	
+	next.pack()
+	play.pack()
+	pause.pack()
+	openSong.pack()
+	window.mainloop()
+
 #pyglet.app.run()
-
-
-next.pack()
-play.pack()
-pause.pack()
-openSong.pack()
-window.mainloop()
